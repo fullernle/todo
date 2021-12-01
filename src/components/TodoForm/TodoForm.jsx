ccebb87ee8f2a33baaa3899738f8a221db4e2c53
lefuller <fullernle@gmail.com>
2021-12-01 13:59:58 -0800

    add basic todo form

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -2,10 +2,17 @@ import React, { useState } from 'react'
 import "./todoform.css";
 
 const TodoForm = () => {
+	const [ input, setInput ] = useState('');
 	return (
-		<div>
-			
-		</div>
+		<form className="todo-form">
+			<input  type="text" 
+							className="todo-input"
+							placeholder="Add a todo!" 
+							value={input}
+							name='text'
+			/>
+			<button className="todo-button">Add</button>
+		</form>
 	)
 }
 
