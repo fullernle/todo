e30009ba9d9ce69b788ca822873e029549b4b8a2
lefuller <fullernle@gmail.com>
2021-12-01 14:33:34 -0800

    implement addTodo and isEmpty for type checking

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,16 +1,30 @@
-import React, { useState }  from 'react'
+import React, { useState } from "react";
 import "./todolist.css";
 import { TodoForm } from "../index";
 
 const TodoList = () => {
-	const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState([]);
 
-	return (
-		<div>
-			<h1>Today's Todo List!</h1>
-			<TodoForm /> 
-		</div>
-	)
-}
+  const isEmpty = (string) => {
+		const substr = string.split("");
+    return substr.length === 0 || substr.every((str) => str === " ");
+  };
 
-export default TodoList
+  const addTodo = (todo) => {
+    if (!todo.text || isEmpty(todo.text)) {
+      return;
+    }
+
+    const newTodoList = [todo, ...todoList];
+    setTodoList(newTodoList);
+  };
+
+  return (
+    <div>
+      <h1>Today's Todo List!</h1>
+      <TodoForm onSubmit={addTodo} />
+    </div>
+  );
+};
+
+export default TodoList;
