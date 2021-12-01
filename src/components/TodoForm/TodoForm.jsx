ddd0bc3d8e7ce376eabef21db4a3b60848e6f526
lefuller <fullernle@gmail.com>
2021-12-01 14:04:52 -0800

    implement handleChange and handleSubmit skeleton

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -3,13 +3,25 @@ import "./todoform.css";
 
 const TodoForm = () => {
 	const [ input, setInput ] = useState('');
+
+	const handleChange = (e) => {
+		setInput(e.target.value);
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		setInput('');
+	}
+
 	return (
-		<form className="todo-form">
+		<form className="todo-form" onSubmit={handleSubmit}>
 			<input  type="text" 
 							className="todo-input"
 							placeholder="Add a todo!" 
 							value={input}
 							name='text'
+							onChange={handleChange}
 			/>
 			<button className="todo-button">Add</button>
 		</form>
